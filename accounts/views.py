7d81b07cac1e25076e9fc8f755d79298d3e4eda7
Talemwa Eria Jackson <120446528+Talemwa-Eria-Jackson@users.noreply.github.com>
2024-05-02 12:58:48 +0300

    added skeleton of home view

--- accounts/views.py
+++ accounts/views.py
@@ -23,3 +23,7 @@ def user_signup(request):
         "form": form,
     }
     return render(request, "accounts/signup.html", context)
+
+
+def home(request):
+    return render(request, "e_commerce/home.html")
