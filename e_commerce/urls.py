d2b8d472af2f09e3984d35c8d6403b6217816e0f
Talemwa Eria Jackson <120446528+Talemwa-Eria-Jackson@users.noreply.github.com>
2024-05-02 13:05:59 +0300

    set up url for home under store app

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+import accounts, store
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("account/", include("accounts.urls")),
+    path("", include("store.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
