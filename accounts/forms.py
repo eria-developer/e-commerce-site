af09d99a298c0e2611554ed7cea9b7017846f9d1
Talemwa Eria Jackson <120446528+Talemwa-Eria-Jackson@users.noreply.github.com>
2024-05-02 12:21:58 +0300

    created signupform

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,2 +1,8 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from . import models
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = models.CustomUser
+        fields = ["firstname", "lastname", "email", "gender"]
