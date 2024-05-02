8cb6345df24f7820be0c5ab062889af20d15b26c
Talemwa Eria Jackson <120446528+Talemwa-Eria-Jackson@users.noreply.github.com>
2024-05-02 12:18:19 +0300

    still implementing authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(models.Model):
+    GENDER_CHOICES = [
+        ("Male", "Male"),
+        ("Female", "Female"),
+    ]
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+    
--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("account/", include("accounts.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
