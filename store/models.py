e4081dfc3f215bbe136d22248edfd9e0e93c046a
Talemwa Eria Jackson <120446528+Talemwa-Eria-Jackson@users.noreply.github.com>
2024-05-03 10:46:44 +0300

    added product and category models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="category_images/")
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    weight = models.DecimalField(decimal_places=2, null=True, blank=True, max_digits=5)
+    color = models.CharField(max_length=30, null=True, blank=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to="product_images")
+
+    def __str__(self):
+        return f"{self.name} at {self.price}"
